0a947dd3b1ba6bf63056d15c67d2e4dc450ce7cd
nparrac1 <npcruz630@gmail.com>
2024-03-23 17:38:17 -0300

    Avance tarea1

--- tarea1.py
+++ tarea1.py
@@ -1,32 +1,45 @@
 import os
-#contenido = os.listdir('Libros')
-#print(f"Las opciones de libro son: {contenido}")
-#libro = input("Ingrese el libro que desea revisar:")
+contenido = os.listdir('Libros')
+for i, libro in enumerate(contenido):
+    print(f"Libro {i} : {libro}")
 
-opcion = ""
+opcion = input("Seleccione el numero del libro que desea analizar: ")
+nombre_libro = contenido[int(opcion)]
 
-while(opcion != "salir"):
-    print("""TAREA 1
-      Seleccione la opcion que desea realizar:
-      1.Contador
-      2.Buscar
-      3.Reemplazar
-      4.Salir
-      """)
-    opcion = input("Ingrese la funcion que desea relizar: ")
-    opcion.lower()
-    if opcion == "contador":
-        print("contar")
+with open(f"Libros/{nombre_libro}", "r", encoding="UTF-8") as Archivo1:
+    ContenidoArchivo1 = Archivo1.read()
+#   print(ContenidoArchivo1)
+# print((ContenidoArchivo1.split()))
+print(len(ContenidoArchivo1.split()))
+print(len(set(ContenidoArchivo1)))
+print(len(ContenidoArchivo1))
+print(len(ContenidoArchivo1.replace(" ","")))
+print(len(ContenidoArchivo1.split("\n")))
 
-    elif opcion == "buscar":
-         print("bucar")
+# opcion = ""
 
-    elif opcion == "reemplazar":
-         print("reemplazar")
+# while(opcion != "salir"):
+#     print("""TAREA 1
+#       Seleccione la opcion que desea realizar:
+#       1.Contador
+#       2.Buscar
+#       3.Reemplazar
+#       4.Salir
+#       """)
+#     opcion = input("Ingrese la funcion que desea relizar: ")
+#     opcion.lower()
+#     if opcion == "contador":
+#         print("contar")
 
-    elif opcion == "salie":
-         print("Saliendoooo") 
-         break    
-    
-    else:
-        print("Por favor selecciona una opcion correcta")
+#     elif opcion == "buscar":
+#          print("bucar")
+
+#     elif opcion == "reemplazar":
+#          print("reemplazar")
+
+#     elif opcion == "salie":
+#          print("Saliendoooo")
+#          break
+
+#     else:
+#         print("Por favor selecciona una opcion correcta")
