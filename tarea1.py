5c29f219654c4c5d82853fe08ce008d091464de7
nparrac1 <npcruz630@gmail.com>
2024-03-24 16:27:08 -0300

    Parte 2
    Se realizo la parte 2 de la tarea 1 de Taller de python para la ciencia de datos

--- tarea1.py
+++ tarea1.py
@@ -3,43 +3,17 @@ contenido = os.listdir('Libros')
 for i, libro in enumerate(contenido):
     print(f"Libro {i} : {libro}")
 
-opcion = input("Seleccione el numero del libro que desea analizar: ")
-nombre_libro = contenido[int(opcion)]
+opcion_libro = input("Seleccione el numero del libro que desea analizar: ")
+nombre_libro = contenido[int(opcion_libro)]
 
 with open(f"Libros/{nombre_libro}", "r", encoding="UTF-8") as Archivo1:
     ContenidoArchivo1 = Archivo1.read()
-#   print(ContenidoArchivo1)
-# print((ContenidoArchivo1.split()))
-print(len(ContenidoArchivo1.split()))
-print(len(set(ContenidoArchivo1)))
-print(len(ContenidoArchivo1))
-print(len(ContenidoArchivo1.replace(" ","")))
-print(len(ContenidoArchivo1.split("\n")))
+    
+palabra = input("Ingrese la palabra que desea buscar: ")
+print(ContenidoArchivo1.lower().count(palabra.lower()))        
+print((ContenidoArchivo1.count(palabra)))
+print(ContenidoArchivo1.lower().find(palabra))  
+print(ContenidoArchivo1.find(palabra))  
 
-# opcion = ""
 
-# while(opcion != "salir"):
-#     print("""TAREA 1
-#       Seleccione la opcion que desea realizar:
-#       1.Contador
-#       2.Buscar
-#       3.Reemplazar
-#       4.Salir
-#       """)
-#     opcion = input("Ingrese la funcion que desea relizar: ")
-#     opcion.lower()
-#     if opcion == "contador":
-#         print("contar")
 
-#     elif opcion == "buscar":
-#          print("bucar")
-
-#     elif opcion == "reemplazar":
-#          print("reemplazar")
-
-#     elif opcion == "salie":
-#          print("Saliendoooo")
-#          break
-
-#     else:
-#         print("Por favor selecciona una opcion correcta")
