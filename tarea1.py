99de3844eb0424d97af2d1e477bee2bbff1a5dea
nparrac1 <103908872+nparrac1@users.noreply.github.com>
2024-03-26 09:50:05 -0300

    Update tarea1.py

--- tarea1.py
+++ tarea1.py
@@ -10,10 +10,11 @@ with open(f"Libros/{nombre_libro}", "r", encoding="UTF-8") as Archivo1:
     ContenidoArchivo1 = Archivo1.read()
     
 palabra = input("Ingrese la palabra que desea buscar: ")
-print(ContenidoArchivo1.lower().count(palabra.lower()))        
-print((ContenidoArchivo1.count(palabra)))
-print(ContenidoArchivo1.lower().find(palabra))  
-print(ContenidoArchivo1.find(palabra))  
+print(f"La palabra completa: {palabra},  sin distinción de mayúsculas y minúsculas aparece{ContenidoArchivo1.lower().replace(",","").count(palabra.lower())}")  
+print(f"La palabra completa: {palabra}, con distinción de mayúsculas y minúsculas aparece{ContenidoArchivo1.replace(",","").count(palabra)}")  
+print(f"La palabra o substring: {palabra}, sin distinción de mayúsculas y minúsculas{ContenidoArchivo1.lower().count(palabra.lower())}")        
+print(f"La palabra o substring: {palabra}, sin distinción de mayúsculas y minúsculas{(ContenidoArchivo1.count(palabra))}")
+
 
 
 
